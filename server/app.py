846e625be4574f4dd2333de5ec70f29ad0628e9b
Joseph Lee <130028511+joeylee08@users.noreply.github.com>
2023-12-04 22:50:34 -0800

    seed data working perfectly

--- server/app.py
+++ server/app.py
@@ -13,13 +13,36 @@ from config import app, db, api
 from models import User, UserListing, Listing, Business
 import secrets
 from dotenv import load_dotenv
+import os
 
+import ipdb
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+app.secret_key = SECRET_KEY
 
 # Views go here!
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+class Login(Resource):
+    def post(self):
+        try:
+            current_id = request.get_json()['user_id']
+            session['current_id'] = current_id
+            ipdb.set_trace()
+            current_user = User.query.filter_by(id=current_id).first().to_dict()
+            return make_response(current_user, 200)
+        except Exception:
+            return make_response({'Error:', 'User not found.'}, 404)
+
+class Logout(Resource):
+    pass
+
+class CurrentUser(Resource):
+    pass
+
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CurrentUser, '/currentuser')
 
 
 if __name__ == '__main__':
